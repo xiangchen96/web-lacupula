2a6f3fe021cea93ded722e1f3f67a8dc66c79495
Xiang Chen <xiangchenchen96@gmail.com>
2020-07-09 00:01:27 +0100

    Remove take away

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -10,8 +10,7 @@ const IndexPage = () => {
   if (typeof window !== `undefined`) {
     lang = window.location.hash
   }
-  if (!["#es", "#en", "#de", "#take", "#dim", "#dessert"].includes(lang))
-    lang = "#es"
+  if (!["#es", "#en", "#de", "#dim", "#dessert"].includes(lang)) lang = "#es"
   lang = lang.substring(1)
   return (
     <Layout>
@@ -58,14 +57,6 @@ const IndexPage = () => {
             Desserts
           </a>
         </li>
-        <li className={`${lang === "take" ? "-mb-px" : ""} mr-1`}>
-          <a
-            className={`${commonStyle} ${lang === "take" ? active : inactive}`}
-            href="#take"
-          >
-            Take away
-          </a>
-        </li>
       </ul>
       {["es", "en", "de"].includes(lang) && (
         <img
@@ -75,23 +66,18 @@ const IndexPage = () => {
           src="/drinks.jpg"
         />
       )}
-      {lang !== "take" &&
-        [1, 2, 3, 4].map(k => (
-          <img
-            alt=""
-            key={k}
-            className="xl:w-2/3 mx-auto"
-            src={`/menu-${lang}-${k}.jpg`}
-          />
-        ))}
+      {[1, 2, 3, 4].map(k => (
+        <img
+          alt=""
+          key={k}
+          className="xl:w-2/3 mx-auto"
+          src={`/menu-${lang}-${k}.jpg`}
+        />
+      ))}
 
       {["es", "en", "de"].includes(lang) && (
         <img className="mx-auto" alt="" src="/allergens.jpg" />
       )}
-      {lang === "take" &&
-        [1, 2].map(k => (
-          <img alt="" className="mx-auto" key={k} src={`/menu-${k}.jpg`} />
-        ))}
       {lang === "dim" &&
         [1, 2, 3, 4].map(k => (
           <img
